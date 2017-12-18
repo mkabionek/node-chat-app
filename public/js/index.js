b8dc505c7acac7837f5088b1a7a9c78c3d76c24e
mkabionek <michalkabionek@gmail.com>
2017-12-18 19:46:20 +0100

    add rooms join

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,60 @@
 var socket = io();
+var rooms = [];
+var joinButton = $('#join');
+var sendButton = $('#send');
+var nick = '';
+var room = '';
 
-socket.on('connect', function () {
-  console.log('Connected to server');
 
-  
+joinButton.on('click', function(e){
+    e.preventDefault();
+    $(this).prop('disabled', true);
+    nick = $('[name=name]').val();
+    room = $('[name=room]').val();
+    
+    if(nick.length != 0 && room.length != 0){
+        
+        socket.emit('join', room, function(err){
+            if(err){
+
+            }else{
+                $('#chat').fadeIn(100);
+                $('#chat_choose').fadeOut(100);
+            }
+        });
+    }else{
+        $(this).prop('disabled', false);
+    }
+});
+
+sendButton.on('click', function(e){
+    e.preventDefault();
+    
+    socket.emit('createMessage', {
+        room: room,
+        from: nick,
+        text: $('[name=message]').val()
+    }, function(){
+        console.log('Message sent');
+     });
 
 });
 
-socket.on('disconnect', function () {
-  console.log('Disconnected from server');
+
+socket.on('connect', function() {
+    console.log('display room list', rooms);
 });
+
 
 socket.on('newMessage', function (message) {
-  console.log('newMessage', message);
-  var li = $('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
-  $('#messages').append(li)
+    console.log('newMessage', message);
+    var li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    $('#messages').append(li)
 });
 
-$('#message-form').on('submit', function(e){
-  e.preventDefault();
-  socket.emit('createMessage', {
-    from: 'User',
-    text: $('[name=message]').val()
-  }, function(){
-    console.log('Message sent');
-  });
+
+socket.on('returnRooms', function (message) {
+    console.log(message);
+    rooms = message.rooms;
 });
--- server/server.js
+++ server/server.js
@@ -10,25 +10,37 @@ const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
-
+var rooms = [];
 app.use(express.static(publicPath));
 
 
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    // Welcome new user
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome'));
+    socket.on('join', (room, callback) => {
+        socket.join(room);
 
-    // New user joined
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined.'));
+        socket.emit('newMessage', generateMessage('Admin', 'Welcome'));
+        socket.broadcast.to(room).emit('newMessage', generateMessage('Admin', 'New user joined.'));
+        callback();
+    })
 
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
-        socket.broadcast.emit('newMessage', generateMessage(message.from, message.text));
-        // io.emit('newMessage', generateMessage(message.from, message.text));
+        // socket.broadcast.emit('newMessage', generateMessage(message.from, message.text));
+        io.to(message.room).emit('newMessage', generateMessage(message.from, message.text));
+        // io.sockets.in(message.room).emit('newMessage', generateMessage(message.from, message.text));
+        console.log(message);
         callback();
     });
+
+    socket.on('getRooms', () => {
+        socket.emit('returnRooms', {rooms: rooms});
+    })
+
+    
+
+
     
     socket.on('disconnect', () => {
         console.log('User was disconnected');
